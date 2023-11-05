da3013f151fd4689129e9d9c39dafd36e3de0d15
pmruthyu <pranav.karthik10@gmail.com>
2023-11-05 01:05:56 -0700

    Initial Commit

---
+++ PodPals/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PodPals
+//
+//  Created by Pranav Karthik on 2023-11-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PodPals/PodPalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodPalsApp.swift
+//  PodPals
+//
+//  Created by Pranav Karthik on 2023-11-05.
+//
+
+import SwiftUI
+
+@main
+struct PodPalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
